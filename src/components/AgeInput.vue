f16d69ddddce4b32e56c5f4cd5a973b30aeadd54
syss69 <art435sys@gmail.com>
2026-01-23 01:25:57 +0100

    The input now is separeted and can be reused in settings

--- src/components/AgeInput.vue
+++ src/components/AgeInput.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ref, watch, computed } from 'vue';
+import { computed, ref } from 'vue';
+import NumericInput from './NumericInput.vue';
 
 const props = defineProps<{
   name: string
@@ -12,60 +13,20 @@ const emit = defineEmits<{
   (e: 'update:modelValue', value: number): void
 }>()
 
-const error = ref<string | null>(null);
+const error = ref<string | null>(null)
 
-const localInputValue = ref<string>(format(props.modelValue))
-
-watch(
-    () => props.modelValue,
-    (newValue) => {
-        localInputValue.value = format(newValue)
+const valueProxy = computed({
+    get: () => props.modelValue,
+    set: (val: number) => {
+        const minHours = props.minAge * 720
+        if (val < minHours){
+            error.value = 'the enteerd age is bbellow the minimum'
+            return
+        }
+        error.value =null
+        emit('update:modelValue', val)
     }
-)
-
-const widths = ['w-[72px]', 'w-[100px]', 'w-[128px]', 'w-[156px]', 'w-[184px]', 'w-[212px]'] 
-
-const widthClass = computed(() => {
-    const len = localInputValue.value.replace(/\s/g, '').length
-    const step = Math.floor(len / 3)
-    return widths[Math.min(step, widths.length - 1)]
 })
-
-/*
-so this part of the code is optional, 
-it possible to do by with using style,
-but I decided to keep only the tailwind, 
-increasing the width by 28 px every three characters.
-i'm blocked here cause tailwind won't accept dynamic ${width}px, so i choose array
- */
-
-function format(value: number | string): string {
-    const digits = value.toString().replace(/\D/g, '')
-    return digits.replace(/\B(?=(\d{3})+(?!\d))/g, ' ') //adding spaces to have a 3digits separated output
-}
-
-function parseToDigit(value: string): number{
-    return Number(value.replace(/\D/g, '')) //now deleting spaces to save value
-}
-
-function onInput(event: Event) {
-    const inputValue = (event.target as HTMLInputElement).value
-
-    localInputValue.value = inputValue
-
-    const inputNumber = parseToDigit(inputValue)
-    localInputValue.value = format(inputNumber)
-
-    //earlier was check if input have a text and if yes shows error, but i decided to just ignore not numbers
-
-    if(inputNumber < Math.floor(props.minAge * 720)){
-        error.value = 'The entered age is below the minimum'
-        return
-    }
-    error.value = null
-    emit('update:modelValue', inputNumber)
-
-}
 </script>
 
 <template>
@@ -79,17 +40,7 @@ function onInput(event: Event) {
     </label>
 
     <div class="flex items-center gap-2">
-      <input
-        :id="inputId"
-        type="text"
-        :value="localInputValue"
-        @input="onInput"
-        :class="[
-            'border border-gray-300 rounded px-2 py-1 text-lg outline-none text-gray-300 group-focus-within:text-indigo-900 group-focus-within:border-violet-700',
-            widthClass
-        ]"
-        placeholder="0"
-      />
+      <NumericInput v-model="valueProxy" :inputId="inputId"/>
       <span class="text-indigo-950">hours old</span>
     </div>
     <p v-if="error" class="text-red-500 text-sm mt-1">
